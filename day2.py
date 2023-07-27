6f6a1db42cffa94adefe7d1b223b024bb1dba074
AleenaVals <112818466+AleenaVals@users.noreply.github.com>
2023-07-27 17:49:53 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 17:50:09 2023
+
+@author: aleen
+"""
+
+'''Create a variable num and assign an integer value of your choice. 
+Print the data type of num using the type() function.'''
+
+num =5
+
+print(type(num))
